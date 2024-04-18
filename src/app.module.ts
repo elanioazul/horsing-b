7fe04ef088b98a88117f4bcf4398faa53a884726
Hugo Herrador <hugo.herrador@inetum.com>
2024-04-18 15:09:43 +0200

    generate base REST API coffees&users resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { CoffeesModule } from './coffees/coffees.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, CoffeesModule],
   controllers: [AppController],
   providers: [AppService],
 })
