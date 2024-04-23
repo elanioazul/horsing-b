cd0939eb5c565955f8d184e02e1558571b0825cc
Hugo Herrador <hugo.herrador@inetum.com>
2024-04-23 09:14:47 +0200

    native hotreload replace docker-compose hot reload but not sure works correctly

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,15 @@ import { ValidationPipe } from '@nestjs/common';
 
 dotenv.config();
 
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.NEST_PORT);
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
